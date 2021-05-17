a1005e3ffeaa0d9b56f44500fa8a19d592839c8e
315 <315@kp15.lan>
2021-05-17 15:38:32 +0300

    Добавьте файлы проекта.

---
+++ DeMo/Арендатор.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeMo
+{
+    public partial class Арендатор : Form
+    {
+        public Арендатор()
+        {
+            InitializeComponent();
+        }
+
+        private void Арендатор_Load(object sender, EventArgs e)
+        {
+          //  арендаторыBindingSource.DataSource = db.Арендаторы.ToList();
+        }
+    }
+}
